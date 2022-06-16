dac690012c526538af4837a5df72c8b464a53329
CCrocilla <cla.crocilla@gmail.com>
2022-06-16 00:24:57 +0000

    Add Login/Logout and Signup Django Functionality

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # The first slug is call path converter while the second is a keyword name. 
+    # The slug keyword name matches the slug parameter in the get method of the 
+    # PostDetails class in the blog/view.py file.
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
 ]
